f7b76d4197d21978f3e370ff71edeff546cd8b3f
GrassWhooper <mutrz@outlook.com>
2016-07-15 22:49:40 +0300

    My NetWork Manager _2

---
+++ Assets/Scripts/MyNetWorkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetWorkManager : NetworkManager {
+    public void MyStartHost()
+    {
+        Debug.Log("should start my host at : " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host Started At" + Time.timeSinceLevelLoad);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
